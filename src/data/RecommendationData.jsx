a1fa55765a460d299c5403768484e9fb1ef248a3
kritijaiswal9051 <krishj9051@gmail.com>
2025-03-25 18:43:02 +0530

    Resolved merge conflicts

--- src/data/RecommendationData.jsx
+++ src/data/RecommendationData.jsx
@@ -1,48 +1,28 @@
-<<<<<<< HEAD
-=======
-import img from "../assets/propertyi.png";
-
->>>>>>> 000c5e7 (update website)
 export const recommendationsData = {
   title: "RECOMMENDED",
   properties: [
     {
       title: "PURI DIPLOMATIC RESIDENCES",
       address: ["SECTOR 111, DWARKA EXPRESSWAY"],
-<<<<<<< HEAD
-      image: "propertyi.png"
-=======
-      image: img, // Uses the imported image
->>>>>>> 000c5e7 (update website)
+
+      image: "propertyi.png",
     },
     {
       title: "TRIBECA ROYAL RESIDENCES",
       address: ["SECTOR 150, GOLF COURSE"],
-<<<<<<< HEAD
-      image: "propertyi.png"
-=======
-      image: img,
->>>>>>> 000c5e7 (update website)
+
+      image: "propertyi.png",
     },
     {
       title: "CONSCIENT PARD",
       address: ["SECTOR 80, NORTHERN PERIPHERAL"],
-<<<<<<< HEAD
-      image: "propertyi.png"
-=======
-      image: img,
->>>>>>> 000c5e7 (update website)
+      image: "propertyi.png",
     },
     {
       title: "OXIRICH CHINTAMANIS",
       address: ["SECTOR 103, DWARKA EXPRESSWAY"],
-<<<<<<< HEAD
-      image: "propertyi.png"
-    }
-  ]
-=======
-      image: img,
+
+      image: "propertyi.png",
     },
   ],
->>>>>>> 000c5e7 (update website)
 };
