3e6377d8c6ee33a0c1a8de01b1141d319f74d73e
Vishal9637 <gadagevishal9@gmail.com>
2025-04-01 22:11:22 +0530

    updated web content

---
+++ client/src/Context/PropertyContext.js
@@ -0,0 +1,30 @@
+// src/contexts/PropertyContext.js
+import React, { createContext, useContext, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllProperties } from "../utils/Store/slice/propertySlice";
+
+const PropertyContext = createContext();
+
+export const PropertyProvider = ({ children }) => {
+  const dispatch = useDispatch();
+  const { properties, loading, error } = useSelector((state) => state.property);
+
+  useEffect(() => {
+    dispatch(getAllProperties());
+  }, [dispatch]);
+
+  return (
+    <PropertyContext.Provider
+      value={{
+        properties,
+        loading,
+        error,
+        refresh: () => dispatch(getAllProperties()),
+      }}
+    >
+      {children}
+    </PropertyContext.Provider>
+  );
+};
+
+export const useProperties = () => useContext(PropertyContext);
