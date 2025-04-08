9e2cd0f837478c099479e926799332088237bdaf
Gagan956 <85867572+Gagan956@users.noreply.github.com>
2025-04-08 17:48:00 +0530

    ignore .env

--- server/src/middlware/authMiddlware.js
+++ server/src/middlware/authMiddlware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.cookies.token || req.headers?.authorization?.split(" ")[1]; // 🔍 Capital 'T'
+    const token = req.cookies.token || req.headers?.authorization?.split(" ")[1]; 
 
     if (!token) {
       return res.status(401).json({
